094cc4cb81cd4f452bd6b365a30487f783acb0db
andaro74 <andaro74@gmail.com>
2025-09-03 19:23:30 -0700

    Structured the code to support multiple microservices

--- src/services/ticketingservice/src/handlers/getTickets.ts
+++ src/services/ticketingservice/src/handlers/getTickets.ts
@@ -10,7 +10,7 @@ import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
  *
  */
 
-export const lambdaHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+export const handler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
     try {
         return {
             statusCode: 200,
